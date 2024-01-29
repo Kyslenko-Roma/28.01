c7acd329cce485518ae9edb57143073ce7cbbabb
Kyslenko-Roma <112819019+Kyslenko-Roma@users.noreply.github.com>
2024-01-29 18:57:31 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-let nameUser = prompt('Enter your name');
+const nameUser = prompt('Enter your name');
 
-if (nameUser === '' && nameUser === null) {
+if (nameUser === '' || nameUser === null) {
     console.log('Error');
-} else if  (nameUser !== '' && nameUser !== null) {
+} else {
     console.log(`Your name is ${nameUser}`);
 
     const ageUser = prompt('Enter your age');
 
-    if (ageUser < 1 && ageUser > 150) {
+    if (ageUser === '' || ageUser === null || !isNaN(+ageUser)) {
         console.log('Error');
-    } else if (ageUser === '' && ageUser === null) {
+    } else if (ageUser <= 1 || ageUser >= 150) {
         console.log('Error');
-    } else if (ageUser > 1 && ageUser < 150) {
+    } else {
         console.log(`Your age is ${+ageUser}`);
 
         const messageUser = prompt('Enter your message');
 
-        if (ageUser <= 18 && messageUser === 'fuck') {
+        if (ageUser < 18 && messageUser === 'fuck') {
             console.log('Не лайся');
         } else {
             console.log(`Your message is ${messageUser}`)
